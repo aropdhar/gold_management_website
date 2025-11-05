7c7dc410994b6bb9db7cf2b35109d1bcafab1fdd
Arop Sutra Dhar <142885013+aropdhar@users.noreply.github.com>
2025-11-05 22:08:29 +0600

    update

--- src/pages/home/feature/Feature.jsx
+++ src/pages/home/feature/Feature.jsx
@@ -1,5 +1,9 @@
 import React, { useState } from 'react'
 import ProductHeading from '../../../component/productheading/ProductHeading'
+import product from '../../../component/productcomponent/productApi/ProductApi'
+import ProductCard from '../../../component/productcomponent/productcard/ProductCard'
+import Slider from "react-slick";
+
 
 const Feature = () => {
     
@@ -7,13 +11,21 @@ const Feature = () => {
 
     const tabs = ['New Arrival' , 'Feature' , 'Best Seller']
 
+      const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 4,
+        slidesToScroll: 3
+      };
+
   return (
     <div className='mb-20'>
       <div className='container'>
          <ProductHeading title={'Feature Product'}/>
          <div className='mt-2 flex items-center gap-x-3 text-[20px]  text-[#777] justify-center'>
             {tabs.map((tab)=>(
-               <button key={tab} onClick={()=>setIsActive(tab)} className={`cursor-pointer border-b-2  transition-all duration-300 ${isActive == tab ? "text-black border-b-2 text-[20px] border-black" : "text-[#777] border-0 border-transparent"}`}>{tab}</button>
+               <button key={tab} onClick={()=>setIsActive(tab)} className={`cursor-pointer border-b-2  transition-all duration-500 ${isActive == tab ? "text-black dark:text-[#777] border-b-2 text-[20px] border-black dark:border-white" : "text-[#777] border-0 border-transparent"}`}>{tab}</button>
             ))}
          </div>
          {isActive == "New Arrival" &&
@@ -23,15 +35,19 @@ const Feature = () => {
          }
 
          {isActive == "Feature" &&
-
-          "Feature"
-
+            <Slider {...settings}>
+                {product.map((item)=>(
+                <ProductCard itemData={item}/>
+                ))}
+            </Slider>
          }
 
          {isActive == "Best Seller" &&
-
-          "Best Seller"
-
+            <Slider {...settings}>
+                {product.map((item)=>(
+                <ProductCard itemData={item}/>
+                ))}
+            </Slider>
          }
       </div>
     </div>
