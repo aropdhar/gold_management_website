f8c59e435718b40056510a8ac4952c0eb869a1cd
Arop Sutra Dhar <142885013+aropdhar@users.noreply.github.com>
2025-11-17 14:17:08 +0600

    all_update

--- src/pages/home/feature/Feature.jsx
+++ src/pages/home/feature/Feature.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import ProductHeading from '../../../component/productheading/ProductHeading'
 import product from '../../../component/productcomponent/productApi/ProductApi'
 import ProductCard from '../../../component/productcomponent/productcard/ProductCard'
@@ -9,12 +9,11 @@ import { SlArrowLeft, SlArrowRight } from 'react-icons/sl';
 const Feature = () => {
     
     const [isActive , setIsActive] = useState("New Arrival")
-
+    const [ishovering , setIshovering] = useState(false);
     const tabs = ['New Arrival' , 'Feature' , 'Best Seller']
     
     
-
-      const settings = {
+    const settings = {
         dots: true,
         infinite: true,
         speed: 500,
@@ -23,10 +22,21 @@ const Feature = () => {
         autoplay: true,
         autoplaySpeed: 3000,
         arrows: false
-      };
+    };
+
+    const sliderref = useRef(null);
+
+    const handleprev = () =>{
+      sliderref.current.slickPrev();
+    }
+
+    const handlenext = () =>{
+      sliderref.current.slickNext();
+    }
+    
 
   return (
-    <div className='mb-20'>
+    <div className='mb-20' onMouseEnter={()=>setIshovering(true)} onMouseLeave={()=>setIshovering(false)}>
       <div className='container'>
          <ProductHeading title={'Feature Product'}/>
          <div className='mt-2 flex items-center gap-x-3 text-[20px]  text-[#777] justify-center'>
@@ -36,17 +46,17 @@ const Feature = () => {
          </div>
          {isActive == "New Arrival" &&
            <div className='relative'>
-               <Slider {...settings}>
+               <Slider ref={sliderref} {...settings}>
                   {product.map((item)=>(
                      <ProductCard itemData={item}/>
                   ))}
                </Slider>
 
-               <div className={`cursor-pointer text-[#777777b8] `}>
-                  <div className='absolute -left-15 top-45'>
+               <div className={`cursor-pointer text-[#777777b8] transition-all duration-300 ${isActive == "New Arrival" && ishovering ? "block" : "hidden"}`}>
+                  <div className='absolute -left-15 top-45' onClick={handleprev}>
                     <span className='inline-block text-[30px]'><SlArrowLeft /></span>
                   </div>
-                  <div className='absolute -right-15 top-45'>
+                  <div className='absolute -right-15 top-45' onClick={handlenext}>
                     <span className='inline-block text-[30px]'><SlArrowRight /></span>
                   </div>
                </div>
@@ -54,19 +64,40 @@ const Feature = () => {
          }
 
          {isActive == "Feature" &&
-            <Slider {...settings}>
-                {product.map((item)=>(
-                  <ProductCard itemData={item}/>
-                ))}
-            </Slider>
+           <div className='relative'>
+                <Slider ref={sliderref} {...settings}>
+                    {product.map((item)=>(
+                      <ProductCard itemData={item}/>
+                    ))}
+                </Slider>
+
+                <div className={`cursor-pointer text-[#777777b8] transition-all duration-300 ${isActive == "Feature" && ishovering ? "block" : "hidden"}`}>
+                      <div className='absolute -left-15 top-45' onClick={handleprev}>
+                        <span className='inline-block text-[30px]'><SlArrowLeft /></span>
+                      </div>
+                      <div className='absolute -right-15 top-45' onClick={handlenext}>
+                        <span className='inline-block text-[30px]'><SlArrowRight /></span>
+                      </div>
+                </div>
+           </div>
          }
 
          {isActive == "Best Seller" &&
-            <Slider {...settings}>
+          <div className='relative'>
+            <Slider ref={sliderref} {...settings}>
                 {product.map((item)=>(
                   <ProductCard itemData={item}/>
                 ))}
             </Slider>
+            <div className={`cursor-pointer text-[#777777b8] transition-all duration-300 ${isActive == "Best Seller" && ishovering ? "block" : "hidden"}`}>
+                  <div className='absolute -left-15 top-45' onClick={handleprev}>
+                    <span className='inline-block text-[30px]'><SlArrowLeft /></span>
+                  </div>
+                  <div className='absolute -right-15 top-45' onClick={handlenext}>
+                    <span className='inline-block text-[30px]'><SlArrowRight /></span>
+                  </div>
+               </div>
+          </div>
          }
       </div>
     </div>
